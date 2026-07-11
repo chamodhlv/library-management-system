b957d2bac846206736db65ad672922dc40248165
chamodhlv <chamodh300@gmail.com>
2026-07-12 04:10:59 +0530

    Implement MemberRepository interface extending JpaRepository for member entity management

--- src/main/java/com/chamodh/library_management_system/repository/MemberRepository.java
+++ src/main/java/com/chamodh/library_management_system/repository/MemberRepository.java
@@ -1,4 +1,11 @@
 package com.chamodh.library_management_system.repository;
 
-public interface MemberRepository {
+import com.chamodh.library_management_system.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+    // Critical for login later - Spring Security will use this to find
+    // a member by the email they type in
 }
