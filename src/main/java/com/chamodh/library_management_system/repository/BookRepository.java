9949aef7998eec34b8ba253adc340d4ee677eff8
chamodhlv <chamodh300@gmail.com>
2026-07-12 04:05:01 +0530

    Refactor BookRepository and CategoryRepository to extend JpaRepository and add query methods

--- src/main/java/com/chamodh/library_management_system/repository/BookRepository.java
+++ src/main/java/com/chamodh/library_management_system/repository/BookRepository.java
@@ -1,4 +1,19 @@
 package com.chamodh.library_management_system.repository;
 
-public interface BookRepository {
+import com.chamodh.library_management_system.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Optional<Book> findByIsbn(String isbn);
+
+    List<Book> findByTitleContainingIgnoreCase(String title);
+    // "Containing" = SQL LIKE %title% (partial match)
+    // "IgnoreCase" = case-insensitive search
+    // Useful for a search bar - user types "harry" and finds "Harry Potter"
+
+    List<Book> findByAvailableCopiesGreaterThan(int copies);
+    // Handy later for "show only books currently available to borrow"
 }
