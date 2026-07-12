b6995dbee1f3322bd7724fa12235898a717a12f2
chamodhlv <chamodh300@gmail.com>
2026-07-13 00:30:03 +0530

    Add DTO classes for Author, Category, and Member with validation annotations

--- src/main/java/com/chamodh/library_management_system/dto/CategoryRequestDto.java
+++ src/main/java/com/chamodh/library_management_system/dto/CategoryRequestDto.java
@@ -1,4 +1,11 @@
 package com.chamodh.library_management_system.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class CategoryRequestDto {
+
+    @NotBlank(message = "Name is required")
+    private String name;
 }
--- src/main/java/com/chamodh/library_management_system/dto/CategoryResponseDto.java
+++ src/main/java/com/chamodh/library_management_system/dto/CategoryResponseDto.java
@@ -1,4 +1,13 @@
 package com.chamodh.library_management_system.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryResponseDto {
+    private Long id;
+    private String name;
 }
